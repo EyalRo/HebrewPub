f0d9e48dbb79160f80243b90c3e145b9aac64b2e
Stags <eyalro@pm.me>
2022-12-25 23:51:17 -0800

    Now marked as alpha version

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,11 +2,13 @@ import React, { useState } from "react"
 import MenuIcon from "./menu.svg"
 import {OpenMenu} from "./OpenMenu"
 import "./header.scss"
+import Alpha from "./alpha.svg"
 
 const Header = () => {
   const [menuOpen, toggleMenu] = useState(false)
   return (
     <header>
+      <img src={Alpha} alt={'Alpha Version'} id={"alpha"}/>
       <div id="title">🦕הפדיברס העברי הראשון🐇</div>
       <div id={"menuButton"}>
         <button onClick={() => toggleMenu(!menuOpen)}>
