6a5179b8042d0fb92916b6d695123fea816ad09c
Stags <eyalro@pm.me>
2022-12-25 23:34:18 -0800

    added reshet.social and toot.org.il to server list

--- src/components/siteList.js
+++ src/components/siteList.js
@@ -1,8 +1,10 @@
 export const siteList = [
-    "tooot.im",
-    "hed.im",
-    "kishkush.net",
-    "hayu.sh",
-    "leftodon.social",
-    "pirsamti.com",
-  ]
+  "tooot.im",
+  "hed.im",
+  "kishkush.net",
+  "hayu.sh",
+  "leftodon.social",
+  "pirsamti.com",
+  "reshet.social",
+  "toot.org.il",
+]
