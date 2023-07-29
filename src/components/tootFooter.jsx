fdaf329fb5701becd7b5490a7b8da16fa2f38125
Stags <stags@isdino.com>
2023-07-28 20:20:21 -0700

    displays traditional icons  when logged in

--- src/components/tootFooter.jsx
+++ src/components/tootFooter.jsx
@@ -1,8 +1,62 @@
-import { Box, CardFooter, Text } from "grommet";
-import React from "react";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Box, CardFooter, Text, Stack, Button } from "grommet";
+import { Favorite, Next, Cycle } from "grommet-icons";
 
-const TootFooter = (toot) => {
-  return (
+const TootFooter = (payload) => {
+  const [liked, setLiked] = useState(false);
+
+  const loginToken = useSelector((state) => state.allToots.loginToken);
+  const loggedIn = loginToken != null;
+  const toot = payload.toot;
+  return loggedIn ? (
+    <CardFooter margin={{ top: "medium" }}>
+      <Box direction="row" width="medium" justify="evenly">
+        {/* Like/Star/Heart */}
+        <Button
+          onClick={() => {
+            setLiked(!liked);
+          }}
+        >
+          <Stack anchor="bottom-right">
+            <Favorite size={liked?"large":"medium"} color="Red" />
+            <Box
+              margin={{ horizontal: "small" }}
+              pad={{ horizontal: "xsmall" }}
+              background="brand"
+              round
+            >
+              <Text size="small">{toot.favourites_count}</Text>
+            </Box>
+          </Stack>
+        </Button>
+        {/* Replies/Threads/Posts */}
+        <Stack anchor="bottom-right">
+          <Next size="medium" />
+          <Box
+            margin={{ horizontal: "small" }}
+            pad={{ horizontal: "xsmall" }}
+            background="brand"
+            round
+          >
+            <Text size="small">{toot.replies_count}</Text>
+          </Box>
+        </Stack>
+        {/* Reblogs/Retoot */}
+        <Stack anchor="bottom-right">
+          <Cycle size="medium" />
+          <Box
+            margin={{ horizontal: "small" }}
+            pad={{ horizontal: "xsmall" }}
+            background="brand"
+            round
+          >
+            <Text size="small">{toot.reblogs_count}</Text>
+          </Box>
+        </Stack>
+      </Box>
+    </CardFooter>
+  ) : (
     <CardFooter margin={{ top: "medium" }}>
       <Box direction="row" width="medium" justify="evenly">
         <Text>{`חיבובים: ${toot.favourites_count}`}</Text>
